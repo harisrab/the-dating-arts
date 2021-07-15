8dda0691cdebc9643e59be25c4509f724567774f
Haris Rashid <harisrab01@gmail.com>
2021-07-15 17:52:48 +0500

    Made Responsive At Home Trainings + Live Exp. + About TDA + About Colgate

--- src/pages/atHomeTrainings/AtHomeSection.js
+++ src/pages/atHomeTrainings/AtHomeSection.js
@@ -39,6 +39,10 @@ const Wrapper = styled.div`
 	position: relative;
 
 	scroll-snap-align: start;
+
+	@media only screen and (max-device-width: 480px) {
+		flex-direction: column;
+	}
 `;
 
 const ContentWrapper = styled.div`
@@ -51,6 +55,12 @@ const ContentWrapper = styled.div`
 	pointer-events: all !important;
 	user-select: text !important;
 	--webkit-user-select: text !important;
+
+	@media only screen and (max-device-width: 480px) {
+		flex-direction: column;
+		width: 100%;
+		height: 60%;
+	}
 `;
 
 const Left = styled.div`
@@ -84,6 +94,22 @@ const Left = styled.div`
 			--webkit-user-select: text !important;
 		}
 	}
+
+	@media only screen and (max-device-width: 480px) {
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		height: 30%;
+
+		.left-content {
+			margin-left: 0px;
+			width: 85%;
+
+			h2 {
+				font-size: 30px;
+			}
+		}
+	}
 `;
 
 const Right = styled.div`
@@ -106,4 +132,18 @@ const Right = styled.div`
 	}
 	height: 100%;
 	width: 60%;
+
+	@media only screen and (max-device-width: 480px) {
+		width: 100%;
+		height: 60%;
+		justify-content: center;
+		align-items: center;
+
+		p {
+			width: 85%;
+			font-size: 15px;
+			height: 200px;
+			overflow-y: scroll;
+		}
+	}
 `;
--- src/pages/liveExperiences/LiveIntroduction.js
+++ src/pages/liveExperiences/LiveIntroduction.js
@@ -41,6 +41,10 @@ const Wrapper = styled.div`
 
 	pointer-events: all;
 	user-select: text;
+
+	@media only screen and (max-device-width: 480px) {
+		flex-direction: column;
+	}
 `;
 
 const ContentWrapper = styled.div`
@@ -52,6 +56,12 @@ const ContentWrapper = styled.div`
 
 	pointer-events: all;
 	user-select: text;
+
+	@media only screen and (max-device-width: 480px) {
+		flex-direction: column;
+		width: 100%;
+		height: 60%;
+	}
 `;
 
 const Left = styled.div`
@@ -78,6 +88,22 @@ const Left = styled.div`
 			user-select: text;
 		}
 	}
+
+	@media only screen and (max-device-width: 480px) {
+		width: 100%;
+		align-items: center;
+		justify-content: center;
+		height: 30%;
+
+		.left-content {
+			margin-left: 0px;
+			width: 85%;
+
+			h2 {
+				font-size: 35px;
+			}
+		}
+	}
 `;
 
 const Right = styled.div`
@@ -99,4 +125,18 @@ const Right = styled.div`
 	}
 	height: 100%;
 	width: 60%;
+
+	@media only screen and (max-device-width: 480px) {
+		width: 100%;
+		height: 60%;
+		justify-content: center;
+		align-items: center;
+
+		p {
+			width: 85%;
+			font-size: 15px;
+			height: 200px;
+			overflow-y: scroll;
+		}
+	}
 `;
